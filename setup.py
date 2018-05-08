e53ed84a316194b37353a55c4d5d779067029d71
Kimitaka <kimitaka01@gmail.com>
2018-05-08 23:50:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ setup(
     packages=['topic_model_intrusion_eval'],
     url='https://github.com/Kiminaka/topic_model_intrusion_eval/topic_model_intrusion_eval',
     author='Kimitaka Nakazawa',
-    author_email='kimitaka01@gmail.com',
     maintainer='Kimitaka Nakazawa',
-    maintainer_email='kimitaka01@gmail.com',
     description='Evaluation using intrusion method for topic models',
     long_description='Create necessary datasets and calculate word intrusion and topic intrusion.',
     download_url='https://github.com/Kiminaka/topic_model_intrusion_eval/topic_model_intrusion_eval'
